061f477e1ebc9facd3d2e0ca08e396d0d27acb50
zendil <zendil@users.noreply.github.com>
2017-06-04 00:58:46 -0400

    create function, set initial triggers

--- Crewk_Moan.lua
+++ Crewk_Moan.lua
@@ -1,6 +1,31 @@
 --Created by Zendil-The Underbog (US) - 03 June 2017
 --Define locals
-local spellids = [
-	
-]
-print('It\'s working!')
+local f = Crewk_Moan_Frame
+local names = {
+	["Crewk"] = true,
+}
+local spells = {
+	["Revival"] = true,
+}
+--Register Events
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+f:SetScript("OnEvent", f.event)
+--Define Function
+function f:event(event, ...)
+	--If combat lof event
+	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
+		local _, eventType, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, _, _, _, _, ... = ...
+		--and spell cast success
+		if eventType == "SPELL_CAST_SUCCESS" then
+			local spellId, spellName, _ = ...
+			--If caster was in names list
+			if names[sourceName] then
+				--and spell was in spells list
+				if spells[spellName] then
+					--play the moan
+					PlaySoundFile("Interface\\Addons\\Crewk_Moan\\media\\crewk_moan.ogg","Master")
+				end
+			end
+		end
+	end
+end
